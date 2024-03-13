4f49c7c00e04012846e02c95dd530ab56a6e5546
Juan Carlos Valle De La Cruz <jvalle-d@c1r9s2.42malaga.com>
2024-03-13 19:40:31 +0100

    argc argv

---
+++ Apuntes/ft_strcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvalle-d <jvalle-d@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/13 12:45:47 by jvalle-d          #+#    #+#             */
+/*   Updated: 2024/03/13 17:47:39 by jvalle-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+int main(int argc, char *argv[])
+{
+   char dest[50] = "Hola";
+   if(argc != 2)
+	return (0);
+   ft_strcat(dest, argv[1]);
+   printf("%s", dest);
+}
+
